71115e3068c484a9e795f6743b3ce81dcb49ad86
ntxtthomas <apm.tthomas@gmail.com>
2017-09-20 17:54:54 -0500

    introduced TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'foo/bar'
+  get 'static_pages/home' => "static_pages#home"
 
-  get 'foo/baz'
+  get 'static_pages/help' => "static_pages#help"
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+  get 'static_pages/about'
 
   root  'application#hello'
 end
