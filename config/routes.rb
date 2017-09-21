b7a1fa73180d6ce116cf4b4f61c3a3af1859ed68
ntxtthomas <apm.tthomas@gmail.com>
2017-09-20 23:12:18 -0500

    Finish Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
   get 'static_pages/home' => "static_pages#home"
-
   get 'static_pages/help' => "static_pages#help"
+  get 'static_pages/about' => "static_pages#about"
+  get 'static_pages/contact' => "static_pages#contact"
 
-  get 'static_pages/about'
-
-  root  'application#hello'
 end
